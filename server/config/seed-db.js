8520be05f8edddce84ee7788414ac22278e649e0
BRENDEN LAPUZ <91161150+cornbren@users.noreply.github.com>
2024-11-02 19:34:30 -0700

    Filled out createTable and insertTable

--- server/config/seed-db.js
+++ server/config/seed-db.js
@@ -14,13 +14,37 @@ const dropTables = async () => {
 
 const createTables = async () => {
     try {
+        console.log('creating restaurants table...');
+        const createQuery = `
+            CREATE TABLE IF NOT EXISTS restaurants (
+                id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                name TEXT NOT NULL,
+                phone TEXT NOT NULL,
+                address TEXT NOT NULL,
+                photo TEXT
+            );
+        `;
+        await pool.query(createQuery);
+        console.log('created restaurants table');
+
     } catch (error) {
         console.log(error)
     }
 }
 
+
 const insertData = async () => {
     try {
+        console.log('adding initial data...');
+        const insertQuery = `
+            INSERT INTO restaurants (name, phone, address, photo) VALUES ('Dim Sum King', '(415) 555-5555', '1600 Holloway Ave', '/images/dimsumking.jpeg');
+            INSERT INTO restaurants (name, phone, address, photo) VALUES ('Original Joe''s', '(415) 555-5555', '1600 Holloway Ave', '/images/originaljoes.jpeg');
+            INSERT INTO restaurants (name, phone, address, photo) VALUES ('Boulevard Cafe', '(415) 555-5555', '1600 Holloway Ave', '/images/boulevardcafe.jpeg');
+            INSERT INTO restaurants (name, phone, address, photo) VALUES ('Koi Palace', '(415) 555-5555', '1600 Holloway Ave', '/images/koi-palace.jpg');
+            INSERT INTO restaurants (name, phone, address, photo) VALUES ('Val''s', '(415) 555-5555', '1600 Holloway Ave', '/images/vals.jpeg');
+            INSERT INTO restaurants (name, phone, address, photo) VALUES ('Banana Island', '(415) 555-5555', '1600 Holloway Ave', '/images/bananaisland.jpeg');
+        `;
+        await pool.query(insertQuery);
     } catch (error) {
         console.log(error)
     }
