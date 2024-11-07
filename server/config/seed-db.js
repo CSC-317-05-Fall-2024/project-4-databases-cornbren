0a27d5d13bb49c83757e3cf93ee90e7df153a55d
BRENDEN LAPUZ <91161150+cornbren@users.noreply.github.com>
2024-11-06 20:19:27 -0800

    Reviews table added

--- server/config/seed-db.js
+++ server/config/seed-db.js
@@ -4,6 +4,7 @@ import { pool } from './database.js';
 const dropTables = async () => {
     try {
         const dropTablesQuery = `
+            DROP TABLE IF EXISTS reviews;
             DROP TABLE IF EXISTS restaurants;
         `;
         await pool.query(dropTablesQuery);
@@ -22,6 +23,14 @@ const createTables = async () => {
                 phone TEXT NOT NULL,
                 address TEXT NOT NULL,
                 photo TEXT
+            );
+
+            CREATE TABLE IF NOT EXISTS reviews (
+                id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                rating INT NOT NULL,
+                content TEXT NOT NULL,
+                restaurant_id INT NOT NULL,
+                FOREIGN KEY (restaurant_id) REFERENCES restaurants(id) ON DELETE CASCADE
             );
         `;
         await pool.query(createQuery);
@@ -43,6 +52,11 @@ const insertData = async () => {
             INSERT INTO restaurants (name, phone, address, photo) VALUES ('Koi Palace', '(415) 555-5555', '1600 Holloway Ave', '/images/koi-palace.jpg');
             INSERT INTO restaurants (name, phone, address, photo) VALUES ('Val''s', '(415) 555-5555', '1600 Holloway Ave', '/images/vals.jpeg');
             INSERT INTO restaurants (name, phone, address, photo) VALUES ('Banana Island', '(415) 555-5555', '1600 Holloway Ave', '/images/bananaisland.jpeg');
+
+           INSERT INTO reviews (rating, content, restaurant_id) VALUES (8, 'So good!', 1);
+           INSERT INTO reviews (rating, content, restaurant_id) VALUES (5, 'So mid!', 1);
+           INSERT INTO reviews (rating, content, restaurant_id) VALUES (1, 'So bad!', 2);
+           INSERT INTO reviews (rating, content, restaurant_id) VALUES (6, 'So decent!!', 2);
         `;
         await pool.query(insertQuery);
     } catch (error) {
